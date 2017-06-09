5d015a1b715ba4dd4fda022e8e47cc9ff4c9df81
帅海军 <shj@shjoffice.local>
2017-06-09 19:51:36 +0800

    2017年 6月 9日 星期五 19时51分36秒 CST

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -8,7 +8,7 @@ router.get('/', async (ctx, next)=> {
   //取pre_auth_code
   let pre_auth_code = await Pre_auth_code()
   //跳转微信授权扫描二维码
-  await ctx.render('auth',{url:'https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid='+ plat.appid +'&pre_auth_code=' + pre_auth_code + '&redirect_uri=http://api.diandianyy.com/common/weixin/auth/callback'})
+  await ctx.render('auth',{url:'https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid='+ plat.appid +'&pre_auth_code=' + pre_auth_code + '&redirect_uri=http://api.diandianyy.com/util/weixin/auth/callback'})
   // ctx.redirect('https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid='+ plat.appid +'&pre_auth_code=' + pre_auth_code + '&redirect_uri=https://wx.shnavy.com/auth/callback')
 })
 
