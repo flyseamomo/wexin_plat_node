f912fb56012f655a8d81f158ed058ac3f7e49bad
帅海军 <shj@shjoffice.local>
2017-07-03 16:29:25 +0800

    2017年 7月 3日 星期一 16时29分25秒 CST

--- fn/wechat/wechat_user.js
+++ fn/wechat/wechat_user.js
@@ -9,7 +9,7 @@ module.exports = async (appid,openid) => {
   console.log('user',user)
   if (user.obj.length>0) {
     //已注册直接返回该用户的信息
-    return user[0]
+    return user.obj[0]
   }else{
     //未注册则先获取用户基本信息
     let authorizer_access_token = await Authorizer_access_token(appid)
